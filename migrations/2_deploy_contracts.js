d76285cbaa846c7c6faf41d44ef58be58560fedc
aaronyuai <c@crynux.ai>
2024-01-04 12:25:17 +0800

    deploy new task contract with random lib and hamming lib

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,17 @@
 const CrynuxToken = artifacts.require("CrynuxToken");
 const Node = artifacts.require("Node");
 const Task = artifacts.require("Task");
+const Random = artifacts.require("Random");
+const Hamming = artifacts.require("Hamming");
 
 module.exports = async function (deployer) {
+    await deployer.deploy(Random);
+    await Random.deployed();
+    await deployer.deploy(Hamming);
+    await Hamming.deployed();
+
+    await deployer.link(Random, Task);
+    await deployer.link(Hamming, Task);
 
     await deployer.deploy(CrynuxToken);
     const crynuxTokenInstance = await CrynuxToken.deployed();
