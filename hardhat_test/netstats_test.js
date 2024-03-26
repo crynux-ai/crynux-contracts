9d02674d4dfd2bf60c01a0fc729a6967676736ec
aaronyuai <c@crynux.ai>
2024-03-26 13:32:41 -0700

    migrate netstat test

---
+++ hardhat_test/netstats_test.js
@@ -0,0 +1,147 @@
+const { assert, expect } = require("chai");
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+const { Verifier } = require("./utils");
+
+describe("Netstats", () => {
+    var v;
+    beforeEach(async () => {
+        v = new Verifier();
+        await v.init();
+    });
+
+    it("test task count when task is executed immediately", async () => {
+        await v.prepareNetwork();
+        await v.prepareUser(v.user);
+
+        const [taskId, nodeRounds] = await v.prepareTask(v.user, v.accounts);
+        let totalTasks = (await v.netstatsInstance.totalTasks());
+        let runningTasks = (await v.netstatsInstance.runningTasks());
+        let queuedTasks = (await v.netstatsInstance.queuedTasks());
+        assert.equal(totalTasks, 1, "Wrong total tasks");
+        assert.equal(runningTasks, 1, "Wrong running tasks");
+        assert.equal(queuedTasks, 0, "Wrong queued tasks");
+
+        const result = "0x0102030405060708";
+
+        // submit commitment
+        for (let i = 0; i < 3; i++) {
+            const [commitment, nonce] = await v.getCommitment(result);
+            await v.taskInstance.connect(v.accounts[i]).submitTaskResultCommitment(
+                taskId,
+                nodeRounds[v.accounts[i].address],
+                commitment,
+                nonce);
+        }
+
+        // disclose task
+        for (let i = 0; i < 3; i++) {
+            await v.taskInstance.connect(v.accounts[i]).discloseTaskResult(
+                taskId,
+                nodeRounds[v.accounts[i].address],
+                result
+            );
+        }
+
+        await v.taskInstance.connect(v.accounts[0]).reportResultsUploaded(
+            taskId,
+            nodeRounds[v.accounts[0].address]
+        );
+
+        totalTasks = await v.netstatsInstance.totalTasks();
+        runningTasks = await v.netstatsInstance.runningTasks();
+        queuedTasks = await v.netstatsInstance.queuedTasks();
+        assert.equal(totalTasks, 1, "Wrong total tasks");
+        assert.equal(runningTasks, 0, "Wrong running tasks");
+        assert.equal(queuedTasks, 0, "Wrong queued tasks");
+
+    })
+})
+
+describe("Netstats", () => {
+    var v;
+    beforeEach(async () => {
+        v = new Verifier();
+        await v.init();
+    });
+
+    it("test task count when there is task in queue", async () => {
+
+        await v.prepareUser(v.user);
+
+        for (let i = 0; i < 3; i++) {
+            await v.taskInstance.connect(v.user).createTask(
+                0,
+                ethers.solidityPackedKeccak256(["string"], ["task hash"]),
+                ethers.solidityPackedKeccak256(["string"], ["data hash"]),
+                8,
+                ethers.parseUnits("10", "ether"),
+                1,
+            );
+        }
+
+        let totalTasks = await v.netstatsInstance.totalTasks();
+        let runningTasks = await v.netstatsInstance.runningTasks();
+        let queuedTasks = await v.netstatsInstance.queuedTasks();
+        assert.equal(totalTasks, 3, "Wrong total tasks");
+        assert.equal(runningTasks, 0, "Wrong running tasks");
+        assert.equal(queuedTasks, 3, "Wrong queued tasks");
+
+        // join nodes
+        let blockNumber = 0;
+        const gpuName = "NVIDIA GeForce GTX 1070 Ti";
+        const gpuVram = 8;
+        for (let i = 0; i < 3; i++) {
+            const nodeAccount = v.accounts[i];
+            await v.cnxInstance.transfer(nodeAccount, ethers.parseUnits("400", "ether"));
+            await v.cnxInstance.connect(nodeAccount).approve(v.nodeInstance.target, ethers.parseUnits("400", "ether"));
+            await v.nodeInstance.connect(nodeAccount).join(gpuName, gpuVram);
+        }
+
+        const result = "0x0102030405060708";
+
+        for (let i = 0; i < 3; i++) {
+            totalTasks = await v.netstatsInstance.totalTasks();
+            runningTasks = await v.netstatsInstance.runningTasks();
+            queuedTasks = await v.netstatsInstance.queuedTasks();
+            assert.equal(totalTasks, 3, "Wrong total tasks");
+            assert.equal(runningTasks, 1, "Wrong running tasks");
+            assert.equal(queuedTasks, 2 - i, "Wrong queued tasks");
+
+            let taskId;
+            let nodeRounds = {};
+            const events = await v.taskInstance.queryFilter("TaskStarted", blockNumber, "latest");
+
+            for (const event of events) {
+                taskId = event.args.taskId;
+                const nodeAddress = event.args.selectedNode;
+                nodeRounds[nodeAddress] = event.args.round;
+                blockNumber = event.blockNumber;
+            }
+
+            // submit commitment
+            for (let i = 0; i < 3; i++) {
+                const [commitment, nonce] = await v.getCommitment(result);
+                await v.taskInstance.connect(v.accounts[i]).submitTaskResultCommitment(
+                    taskId,
+                    nodeRounds[v.accounts[i].address],
+                    commitment,
+                    nonce)
+            }
+
+            // disclose task
+            for (let i = 0; i < 3; i++) {
+                await v.taskInstance.connect(v.accounts[i]).discloseTaskResult(
+                    taskId,
+                    nodeRounds[v.accounts[i].address],
+                    result,
+                );
+            }
+
+            await v.taskInstance.connect(v.accounts[0]).reportResultsUploaded(
+                taskId,
+                nodeRounds[v.accounts[0].address],
+            );
+
+        }
+    })
+})
---
+++ hardhat_test/utils.js
@@ -0,0 +1,106 @@
+const { assert, expect } = require("chai");
+
+class Verifier {
+    async init () {
+        var [owner, user, ...accounts] = await ethers.getSigners();
+        //accounts = await ethers.getSigners();
+        var cnxInstance = await ethers.deployContract("CrynuxToken");
+        var qosInstance = await ethers.deployContract("QOS");   
+        var netstatsInstance = await ethers.deployContract("NetworkStats");
+        var nodeInstance = await ethers.deployContract(
+            "Node", [cnxInstance, qosInstance, netstatsInstance]);
+        var taskQueueInstance = await ethers.deployContract("TaskQueue");
+        var taskInstance = await ethers.deployContract("Task",
+            [nodeInstance, cnxInstance, qosInstance, taskQueueInstance, netstatsInstance]);
+
+        await netstatsInstance.updateNodeContractAddress(nodeInstance.target);
+        await netstatsInstance.updateTaskContractAddress(taskInstance.target);
+        await qosInstance.updateNodeContractAddress(nodeInstance.target);
+        await qosInstance.updateTaskContractAddress(taskInstance.target);
+        await taskQueueInstance.updateTaskContractAddress(taskInstance.target);
+        await nodeInstance.updateTaskContractAddress(taskInstance.target);
+
+        this.taskInstance = taskInstance;
+        this.cnxInstance = cnxInstance;
+        this.nodeInstance = nodeInstance;
+        this.netstatsInstance = netstatsInstance;
+        this.qosInstance = qosInstance;
+        this.taskQueueInstance = taskQueueInstance;
+        this.accounts = accounts;
+        this.owner = owner;
+        this.user = user;
+    }
+
+    async prepareNetwork(gpuNames = null, gpuVrams = null) {
+        if (!gpuNames) {
+            gpuNames = ["NVIDIA GeForce GTX 1070 Ti", "NVIDIA GeForce GTX 1070 Ti", "NVIDIA GeForce GTX 1070 Ti"]
+        }
+        if (!gpuVrams) {
+            gpuVrams = [8, 8, 8]
+        }
+        assert.equal(gpuNames.length, 3, "gpuNames should have 3 elements")
+        assert.equal(gpuNames.length, gpuVrams.length, "gpuNames length should equal to gpuVrams")
+        for(let i = 0; i < 3; i++) {
+            await this.prepareNode(this.accounts[i], gpuNames[i], gpuVrams[i]);
+        }
+    }
+
+    async prepareNode(nodeAccount, gpuName = "NVIDIA GeForce GTX 1070 Ti", gpuVram = 8) {
+        await this.cnxInstance.transfer(nodeAccount.address, ethers.parseUnits("400", "ether"));
+        await this.cnxInstance.connect(nodeAccount).approve(this.nodeInstance.target, ethers.parseUnits("400", "ether"));
+        var nodeContract = await this.nodeInstance.connect(nodeAccount);
+        await nodeContract.join(gpuName, gpuVram);
+    }
+
+    async prepareUser(userAccount) {
+        await this.cnxInstance.transfer(userAccount.address, ethers.parseUnits("500", "ether"));
+        await this.cnxInstance.connect(userAccount).approve(this.taskInstance.target, ethers.parseUnits("500", "ether"));
+    }
+
+    async prepareTask(user, accounts, taskType = 0, vramLimit = 0) {
+
+        // Create the task.
+
+        const balBefore = await this.cnxInstance.balanceOf(user);
+        const taskFee = ethers.parseUnits("50", "ether");
+
+        let tx = await this.taskInstance.connect(user).createTask(
+            taskType,
+            ethers.solidityPackedKeccak256(["string"], ["task hash"]),
+            ethers.solidityPackedKeccak256(["string"], ["data hash"]),
+            vramLimit,
+            taskFee,
+            1,
+        );
+        tx = await tx.wait();
+        let logs = tx.logs.filter((x) => x.constructor.name == "EventLog");
+        assert.equal(logs.length, 4, "wrong log number");
+
+        const balAfter = await this.cnxInstance.balanceOf(user);
+        assert.equal(balBefore, balAfter + taskFee, "user task fee not paid");
+
+        const taskId = logs[0].args.taskId;
+        let nodeRounds = {};
+
+        for (let i = 1; i < 4; i++) {
+            const nodeAddress = logs[i].args.selectedNode;
+            nodeRounds[nodeAddress] = logs[i].args.round;
+        }
+
+        for (let i = 0; i < 3; i++) {
+            const nodeTaskId = await this.taskInstance.getNodeTask(this.accounts[i]);
+            assert.equal(taskId.toString(), nodeTaskId.toString(), "incorrect node task");
+        }
+
+        return [taskId, nodeRounds];
+    }
+
+
+    async getCommitment(result) {
+        const nonce = ethers.solidityPackedKeccak256(["uint"], [Math.round(Math.random() * 100000000)]);
+        return [ethers.solidityPackedKeccak256(["bytes", "uint"], [result, nonce]), nonce];
+    }
+
+}
+
+module.exports = {Verifier};
